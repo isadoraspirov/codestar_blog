52f830563efd305892f64975bed53a8526f92090
isadoraspirov <dora.isinha2@gmail.com>
2026-04-28 09:38:03 +0100

    Add runtime.txt to use Python 3.11

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_AJy9KNSpWPE0@ep-wispy-dew-alqjk9fk.c-3.eu-central-1.aws.neon.tech/vice_utter_lung_825335")
+os.environ.setdefault("SECRET_KEY", "3ajAd7RGoG0ya5WIIQ55bWHPrQEpxOiSKVA2uqnXbu7")
