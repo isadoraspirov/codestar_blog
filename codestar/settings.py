459f142dae0c675fc3808cf7be7dc51340009834
isadoraspirov <dora.isinha2@gmail.com>
2026-05-10 12:06:23 +0100

    Update requirements and add migrations

--- codestar/settings.py
+++ codestar/settings.py
@@ -49,11 +49,10 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap5',
-    'blog',
     'django_summernote',
     'cloudinary',
     'blog',
-    'about',
+    'about'
 ]
 
 SITE_ID = 1
